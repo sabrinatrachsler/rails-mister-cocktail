67e1ff7fcd7544afb5862fc3d75f77f2b5982b71
Sabrina Trachsler <sabrina.trachsler@hotmail.com>
2020-02-13 14:02:07 +0100

    routes, controllers, view of cocktails

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,18 @@
+class DosesController < ApplicationController
+  # def create
+  #   @cocktail = cocktail.find(params[:cocktail])
+  #   @doses = doses.new(doses)
+  #   @doses.cocktail = @cocktail
+  #   if @doses.save
+  #     redirect_to cocktail_path(@cocktail)
+  #   else
+  #     render "cocktails/show"
+  #   end
+  # end
+
+  # private
+
+  # def doses_params
+  #   params.require(:doses).permit(:description)
+  # end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "ginger beer")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "coca cola")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "ginger")
+Ingredient.create(name: "strawberry")
+Ingredient.create(name: "rasberry")
+Ingredient.create(name: "rasberry")
+Ingredient.create(name: "prosecco")
+
+
+# Cocktail.create(name: "Gin Fizz")
+# Cocktail.create(name: "Rhum Cola")
+# Cocktail.create(name: "Negroni")
+# Cocktail.create(name: "Aperol Spritz")
+
